e93743b02935436458d6886015a0480c1b50cc13
Krzysztof Królikowski <kkrolikowski92@gmail.com>
2019-02-15 13:38:37 +0100

    1.drum - add/remove transitions for keys

--- 1.drum/scripts/drum.js
+++ 1.drum/scripts/drum.js
@@ -1,11 +1,23 @@
-document.onkeydown = function (e) {
-	const keyCode = e.keyCode;
-	const audio = document.querySelector(`audio[data-key='${keyCode}']`)
-	const key = document.querySelector(`div[data-key='${keyCode}']`)
-	
+function removeTransition(e) {
+	if(e.propertyName !== 'transform') return;
+	e.target.classList.remove('playing');
+}
+
+function playSound(e) {
+	const audio = document.querySelector(`audio[data-key='${e.keyCode}']`);
+	const key = document.querySelector(`div[data-key='${e.keyCode}']`);	
+
 	if(!audio || !key){
 		return;
 	}
-	
+
+	key.classList.add("playing");	
+	audio.currentTime = 0;
 	audio.play();
+}
+
+window.onload = function() {
+	const keys = Array.from(document.querySelectorAll('.key'));
+	keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+	window.addEventListener('keydown', playSound);
 }
